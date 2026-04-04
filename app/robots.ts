734719cf1281115e0ecdb80cd411ca4cce5572f2
William-LP <10053686+William-LP@users.noreply.github.com>
2026-04-04 22:26:12 +0200

    fix robots

--- app/robots.ts
+++ app/robots.ts
@@ -2,13 +2,15 @@ import type { MetadataRoute } from "next";
 
 const BASE_URL = "https://zapia.fr";
 
+export const dynamic = "force-static"
+
 export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: `${BASE_URL}/sitemap.xml`,
-    host: BASE_URL,
-  };
+    return {
+        rules: {
+            userAgent: "*",
+            allow: "/",
+        },
+        sitemap: `${BASE_URL}/sitemap.xml`,
+        host: BASE_URL,
+    };
 }
